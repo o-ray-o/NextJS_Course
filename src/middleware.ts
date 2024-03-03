a9f5610b886333fc20e00a45f41cfd132140a8cf
Ray <61679842+o-ray-o@users.noreply.github.com>
2024-03-04 00:25:33 +0800

    First Commit

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+// export const config={
+//     matcher:"profile",
+
+// }
